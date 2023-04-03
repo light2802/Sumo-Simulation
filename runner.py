74740affdf7377aa3c427f125340eb49f9ec8500
Aarya Chaumal <aarya.chaumal@gmail.com>
2023-04-03 22:19:37 +0530

    Add script to get stats

--- runner.py
+++ runner.py
@@ -134,9 +134,9 @@ def get_options():
     optParser = optparse.OptionParser()
     optParser.add_option("--nogui", action="store_true",
                          default=False, help="run the commandline version of sumo")
-    optParser.add_option("--date", action="store_true", default=11,
+    optParser.add_option("--date", action="store", default=11,
                             help="give date for simulation")
-    optParser.add_option("--numClients", action="store_true", default="2",
+    optParser.add_option("--numClients", action="store", default="1",
                             help="Number of clients in simulation")
     options, args = optParser.parse_args()
     return options
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     date = options.date
     # first, generate the route file for this simulation
     generate_networkfile()
-    generate_routefile(date)
+    generate_routefile(int(date))
 
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
